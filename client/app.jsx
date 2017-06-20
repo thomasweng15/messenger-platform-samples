6c822837926344bfbc48335fc2a5b853eabc1e24
Thomas Weng <thomas.weng11@gmail.com>
2017-06-20 01:13:27 -0700

    Added react component stub for webview

---
+++ client/app.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <h1>This is the Reminder App.</h1>
+  }
+};
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,2 +1,33 @@
 
-console.log("hello");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+
+/*
+ * Function for attaching the application when MessengerExtensions has loaded
+ */
+window.attachApp = (viewerId, listId, socketAddress, threadType) => {
+  const apiUri = `https://${window.location.hostname}`;
+  let app;
+  if (viewerId) {
+    app = (
+      // The main show
+      <App
+        viewerId={viewerId}
+        listId={listId}
+        apiUri={apiUri}
+        socketAddress={socketAddress}
+        threadType={threadType}
+      />
+    );
+  } else {
+    /**
+     * MessengerExtensions are only available on iOS and Android,
+     * so show an error page if MessengerExtensions was unable to start
+     */
+    app = <h1>Messenger extensions are only available on iOS and Android</h1>;
+  }
+
+  ReactDOM.render(app, document.getElementById('content'));
+};
